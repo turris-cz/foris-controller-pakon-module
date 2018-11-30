bc7c75ece71b96778df30aa5f525ddee8ab7ac1f
Stepan Henek <stepan.henek@nic.cz>
2018-11-30 13:22:05 +0100

    setup.py: using namespace_packages attribute

--- setup.py
+++ setup.py
@@ -41,7 +41,10 @@ setup(
     package_data={
         'foris_controller_modules.pakon': ['schema', 'schema/*.json'],
     },
-    scripts=[],
+    namespace_packages=[
+        'foris_controller_modules',
+        'foris_controller_backends',
+    ],
     description=DESCRIPTION,
     long_description=open('README.rst').read(),
     install_requires=[
